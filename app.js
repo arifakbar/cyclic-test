2a105ff6771f966b8994c43cf1cb23d51f2228ff
Arif <64827038+arifakbar@users.noreply.github.com>
2023-03-27 20:27:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const socket = require("socket.io");
 require("dotenv").config();
 
 //Routes
